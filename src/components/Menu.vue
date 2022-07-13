5aef315cfe50af9470974b04eafd07637674a3fd
AsakiRain <1287059631@qq.com>
2022-07-13 10:56:30 +0800

    fix: fix router logic

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,18 +1,79 @@
 <template>
-  <el-menu background-color="#f5f5f5" :router="true" id="menu">
-    <el-menu-item index="/">首页</el-menu-item>
-    <el-menu-item index="/stat">统计信息</el-menu-item>
-    <el-menu-item index="/goods">商品选择</el-menu-item>
-    <el-menu-item index="/detail">商品详情</el-menu-item>
-    <el-menu-item index="/about">关于</el-menu-item>
+  <el-menu
+    background-color="#f5f5f5"
+    id="menu"
+    :default-active="activeMenu"
+    :router="true"
+  >
+    <el-menu-item index="/">
+      <el-icon><HomeFilled /></el-icon>
+      <span>首页</span>
+    </el-menu-item>
+    <el-menu-item index="/stat">
+      <el-icon><Histogram /></el-icon>
+      <span>统计信息</span>
+    </el-menu-item>
+    <el-menu-item index="/goods">
+      <el-icon><GoodsFilled /></el-icon>
+      <span>商品选择</span>
+    </el-menu-item>
+    <el-sub-menu index="/detail">
+      <template #title>
+        <el-icon><Menu /></el-icon>
+        <span>商品详情</span>
+      </template>
+      <el-menu-item index="/detail/list" :disabled="!inDetail"
+        >评论列表</el-menu-item
+      >
+      <el-menu-item index="/detail/cloud" :disabled="!inDetail"
+        >词云统计</el-menu-item
+      >
+      <el-menu-item index="/detail/emo" :disabled="!inDetail"
+        >情感分析</el-menu-item
+      >
+    </el-sub-menu>
+    <el-menu-item index="/about">
+      <el-icon><InfoFilled /></el-icon>
+      <span>关于</span>
+    </el-menu-item>
   </el-menu>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import {
+  HomeFilled,
+  Histogram,
+  GoodsFilled,
+  InfoFilled,
+  Menu
+} from '@element-plus/icons-vue';
+import { computed, ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+const inDetail = ref(false);
+
+watch(
+  () => router.currentRoute.value.fullPath,
+  (val: string) => {
+    const paths = val.split('/');
+    if (paths[1] === 'detail') {
+      inDetail.value = true;
+    } else {
+      inDetail.value = true;
+    }
+  }
+);
+const activeMenu = computed(() => {
+  return route.path;
+});
+</script>
 <style scoped>
 #menu {
   flex-grow: 1;
 }
-.el-menu-item {
+.el-menu-item,
+.el-sub-menu__title * {
   font-size: 16px;
 }
 </style>
--- src/views/detail/index.vue
+++ src/views/detail/index.vue
@@ -1,12 +1,17 @@
 <template>
   <el-header>
     <el-page-header :icon="ArrowLeft" @back="goBack">
-    <template #title>
-      <span class="subTitle">返回</span>
-    </template>
+      <template #title>
+        <span class="subTitle">返回</span>
+      </template>
       <template #content>
         <span>
-          <el-menu :router="true" mode="horizontal" :ellipsis="false">
+          <el-menu
+            mode="horizontal"
+            :ellipsis="false"
+            @select="handleSelect"
+            :default-active="activeMenu"
+          >
             <el-menu-item index="/detail/list">评论查看</el-menu-item>
             <el-menu-item index="/detail/cloud">词云统计</el-menu-item>
             <el-menu-item index="/detail/emo">情感分析</el-menu-item>
@@ -19,11 +24,24 @@
 </template>
 <script setup lang="ts">
 import { ArrowLeft } from '@element-plus/icons-vue';
-import { useRouter } from 'vue-router';
-const router = useRouter();
+import { computed } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
 
+const router = useRouter();
+const route = useRoute();
+
+const activeMenu = computed(() => {
+  return route.path;
+});
 const goBack = () => {
   router.back();
+};
+
+const handleSelect = (path: string) => {
+  router.push({
+    path: path,
+    query: route.query
+  });
 };
 </script>
 <style scoped>
@@ -40,5 +58,8 @@ const goBack = () => {
 .el-menu {
   height: 50px;
   background-color: #f5f5f5;
+}
+.el-menu-item {
+  font-weight: bold;
 }
 </style>
