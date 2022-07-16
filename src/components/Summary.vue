63f48f926430e6732b237697de815b35255d1bbb
AsakiRain <1287059631@qq.com>
2022-07-17 03:49:58 +0800

    feat: 基本信息查看

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -1,22 +1,54 @@
 <template>
   <el-main id="summaryBody" :loading="isLoading">
-    {{ data.name }}
+    <el-carousel :interval="3000" arrow="always" id="slider" height="400px">
+      <el-carousel-item v-for="(pic, index) in data.pics" :key="index">
+        <el-image :src="pic" fit="cover" class="sliderImage">
+          <template #placeholder>
+            <div class="placeholderSlot">
+              <el-icon><Picture /></el-icon>
+            </div>
+          </template>
+        </el-image>
+      </el-carousel-item>
+    </el-carousel>
+    <el-descriptions :column="1" :border="true" id="infoWrapper">
+      <template #title>
+        <div id="titleSlot">{{ data.name }}</div>
+      </template>
+      <el-descriptions-item label="商品ID">{{ id }}</el-descriptions-item>
+      <el-descriptions-item label="价格区间">
+        <span id="infoPrice">￥{{ data.price }}</span>
+      </el-descriptions-item>
+      <el-descriptions-item label="颜色分类">
+        <el-popover
+          placement="right-start"
+          title="所有颜色分类"
+          trigger="click"
+        >
+          <template #reference>
+            <el-button>点击查看</el-button>
+          </template>
+          <el-row v-for="type in data.sales" class="tag">
+            <el-tag size="small">
+              {{ type.name }}
+            </el-tag>
+          </el-row>
+        </el-popover>
+      </el-descriptions-item>
+    </el-descriptions>
   </el-main>
 </template>
 <script lang="ts" setup>
 import { ref, toRefs, watch } from 'vue';
 import { apiGet } from '@/api';
-import { ElMessage } from 'element-plus';
+import { Picture } from '@element-plus/icons-vue';
 
 interface data {
   name: string;
   price: string;
-  id: string;
-  status: string;
-  sales: string;
-  picture: string;
-  description: string;
-  descore: string;
+  sales: any;
+  pics: string[];
+  descore: object;
 }
 const props = defineProps({
   id: {
@@ -30,32 +62,65 @@ const isLoading = ref(true);
 const data = ref({
   name: '获取中',
   price: '获取中',
-  id: '获取中',
-  status: '获取中',
-  sales: '[]',
-  picture: '[]',
-  description: '[]',
-  descore: '[]'
+  sales: [],
+  pics: [],
+  description: [],
+  descore: []
 } as data);
 
 watch(
   () => id.value,
   () => {
-    isLoading.value = true;
-    apiGet('/api/good/summary', { id: id.value })
-      .then((res) => {
-        data.value = res.data;
-      })
-      .catch((err) => {
-        isLoading.value = false;
-        return ElMessage.error(err.message);
+    if (id.value != '' && id.value != null) {
+      isLoading.value = true;
+      apiGet('/api/good/summary', { id: id.value }).then((res) => {
+        const rawData = res.data;
+        data.value = {
+          name: rawData.name,
+          price: rawData.price,
+          sales: JSON.parse(rawData.sales),
+          pics: JSON.parse(rawData.picture),
+          descore: JSON.parse(rawData.descore)
+        };
       });
+    }
   },
   { immediate: true }
 );
 </script>
 <style scoped>
 #summaryBody {
-  padding: 0;
+  padding: 10px 20px 0 20px;
+  display: flex;
+  width: 860px;
+}
+#slider {
+  width: 400px;
+}
+.placeholderSlot {
+  height: 400px;
+  width: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.sliderImage {
+  height: 400px;
+  width: 400px;
+  border-radius: 4px;
+}
+#titleSlot {
+  font-size: 24px;
+  font-weight: bold;
+}
+.tag {
+  margin-bottom: 4px;
+}
+#infoWrapper {
+  width: 400px;
+  margin-left: 20px;
+}
+#infoPrice {
+  color: #f5a623;
 }
 </style>
